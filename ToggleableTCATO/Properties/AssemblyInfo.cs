14f33da98180404fea28af91f590cb45191ff466
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2026-04-18 12:14:21 +0800

    Reorganize project

--- ToggleableTCATO/Properties/AssemblyInfo.cs
+++ ToggleableTCATO/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ToggleableTCATO")]
-[assembly: AssemblyDescription("A Keepass plugin to toggle Two-Channel Auto-Type Obfuscation in the middle of auto-typing.")]
+[assembly: AssemblyDescription("A KeePass 2 plugin to toggle Two-Channel Auto-Type Obfuscation in the middle of auto-typing.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CarrieForle")]
 [assembly: AssemblyProduct("KeePass Plugin")]
